fbaa7ca7d370741fef566580d1362ebec0bd89ae
Andrii Vasylchenko <andrvas92@gmail.com>
2021-04-18 23:49:49 -0400

    added sign in components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Route, Switch} from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sing-up/sign-in-and-sing-up.component';
 
 
 
@@ -15,6 +16,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route  path='/shop' component={ShopPage}/>
+      <Route  path='/signin' component={SignInAndSignUpPage}/>
     </Switch>
       
     </div>
